853faef891fa9ba52cfee56d3fab16df28ddfab7
SalOne22 <111443297+SalOne22@users.noreply.github.com>
2023-08-05 20:22:18 +0200

    Added roboto-regular

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 import { useFonts } from 'expo-font';
-import { StatusBar } from 'expo-status-bar';
-import { View } from 'react-native';
 import RegistrationScreen from './src/screens/RegistrationScreen';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     'Roboto-Light': require('./assets/fonts/Roboto-Light.ttf'),
+    'Roboto-Regular': require('./assets/fonts/Roboto-Regular.ttf'),
     'Roboto-Medium': require('./assets/fonts/Roboto-Medium.ttf'),
   });
 
