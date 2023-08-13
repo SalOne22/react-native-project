c98b7b37e5f5163ee62d6e5facc49d25c413e643
SalOne22 <111443297+SalOne22@users.noreply.github.com>
2023-08-13 17:20:13 +0200

    Added base Home screen

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,5 +1,47 @@
-import { Text } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Feather } from '@expo/vector-icons';
+import PostsScreen from './PostsScreen';
+import { LogoutButton } from '../components/LogoutButton';
+
+const Tab = createBottomTabNavigator();
 
 export default function Home() {
-  return <Text>Home</Text>;
+  return (
+    <Tab.Navigator
+      initialRouteName="Posts"
+      screenOptions={{
+        headerStyle: {
+          shadowColor: 'rgba(0 0 0 / 0.3)',
+          shadowOffset: '0px 0.5px 0px 0px',
+        },
+
+        headerTitleAlign: 'center',
+        headerTintColor: '#212121',
+        headerTitleStyle: {
+          fontSize: 17,
+          fontFamily: 'Roboto-Medium',
+          lineHeight: 22,
+          letterSpacing: -0.408,
+        },
+        headerTitleContainerStyle: {
+          paddingVertical: 11,
+        },
+        tabBarShowLabel: false,
+      }}
+    >
+      <Tab.Screen
+        name="Posts"
+        component={PostsScreen}
+        options={{
+          title: 'Публікації',
+          headerRight: () => <LogoutButton />,
+          headerRightContainerStyle: {
+            paddingVertical: 10,
+            right: 16,
+          },
+          tabBarIcon: () => <Feather name="grid" size={24} color="black" />,
+        }}
+      />
+    </Tab.Navigator>
+  );
 }
