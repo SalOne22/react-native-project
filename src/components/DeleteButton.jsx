6e523f4db04eeb9775237eb57bffecb71a811dd0
SalOne22 <111443297+SalOne22@users.noreply.github.com>
2023-08-14 16:53:22 +0200

    Added DeleteButton

---
+++ src/components/DeleteButton.jsx
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { IconButton } from './IconButton';
+
+export const DeleteButton = ({ style }) => {
+  return (
+    <IconButton
+      style={[styles.button, style]}
+      icon={<Feather name="trash-2" size={24} color="#bdbdbd" />}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    width: 70,
+    paddingVertical: 8,
+    borderRadius: 20,
+    alignItems: 'center',
+    backgroundColor: '#f6f6f6',
+  },
+});
