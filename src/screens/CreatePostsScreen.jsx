f43f9977bf78a2054caf614aaabad95d91e7b0ec
SalOne22 <111443297+SalOne22@users.noreply.github.com>
2023-08-14 16:58:49 +0200

    Removed unused dependencies

--- src/screens/CreatePostsScreen.jsx
+++ src/screens/CreatePostsScreen.jsx
@@ -1,4 +1,4 @@
-import { KeyboardAvoidingView, Platform, StyleSheet } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { useForm } from 'react-hook-form';
 import { Feather } from '@expo/vector-icons';
 import { ChangeablePostImage } from '../components/ChangeablePostImage';
