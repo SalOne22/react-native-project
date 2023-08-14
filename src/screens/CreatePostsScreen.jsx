153b998c6af2bcab06f27558a1a3dc98e57474f3
SalOne22 <111443297+SalOne22@users.noreply.github.com>
2023-08-14 16:54:19 +0200

    Added CreatePosts screen

--- src/screens/CreatePostsScreen.jsx
+++ src/screens/CreatePostsScreen.jsx
@@ -1,5 +1,57 @@
-import { Text } from 'react-native';
+import { KeyboardAvoidingView, Platform, StyleSheet } from 'react-native';
+import { useForm } from 'react-hook-form';
+import { Feather } from '@expo/vector-icons';
+import { ChangeablePostImage } from '../components/ChangeablePostImage';
+import { GhostInput } from '../components/GhostInput';
+import { Button } from '../components/Button';
+import { DeleteButton } from '../components/DeleteButton';
+import { Container } from '../components/Container';
+import postImage from '../../assets/images/postImage.jpg';
 
 export default function CreatePostsScreen() {
-  return <Text>CreatePostsScreen</Text>;
+  const { control, handleSubmit, reset } = useForm({
+    defaultValues: {
+      name: '',
+      location: '',
+    },
+  });
+
+  const onSubmit = (data) => {
+    console.log(data);
+    reset();
+    navigation.navigate('Home');
+  };
+
+  return (
+    <Container>
+      {/* Убрать source - появиться пустая картинка */}
+      <ChangeablePostImage style={styles.changeableImage} source={postImage} />
+
+      <GhostInput accent style={styles.topInput} placeholder="Назва..." />
+      <GhostInput
+        style={styles.bottomInput}
+        placeholder="Місцевість..."
+        icon={<Feather name="map-pin" size={24} color="#dbdbdb" />}
+      />
+
+      <Button title="Опублікувати" disabled onPress={handleSubmit(onSubmit)} />
+      <DeleteButton style={styles.deleteButton} onPress={() => reset()} />
+    </Container>
+  );
 }
+
+const styles = StyleSheet.create({
+  changeableImage: {
+    marginBottom: 32,
+  },
+  topInput: {
+    marginBottom: 16,
+  },
+  bottomInput: {
+    marginBottom: 32,
+  },
+  deleteButton: {
+    marginTop: 'auto',
+    alignSelf: 'center',
+  },
+});
