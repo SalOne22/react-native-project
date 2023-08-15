84ca9fdc2a20f98c671492cd0a982b193a4c4e23
SalOne22 <111443297+SalOne22@users.noreply.github.com>
2023-08-15 11:22:52 +0200

    Added post mock data

---
+++ src/mock/posts.js
@@ -0,0 +1,39 @@
+import postImage from '../../assets/images/postImage.jpg';
+
+export const posts = [
+  {
+    id: 1,
+    image: postImage,
+    title: 'Ліс',
+    comments: 0,
+    location: "Ivano-Frankivs'k Region, Ukraine",
+  },
+  {
+    id: 2,
+    image: postImage,
+    title: 'Замок у Карпатах',
+    comments: 2,
+    location: 'Zakarpattia Region, Ukraine',
+  },
+  {
+    id: 3,
+    image: postImage,
+    title: 'Природа Карпат',
+    comments: 5,
+    location: 'Lviv Region, Ukraine',
+  },
+  {
+    id: 4,
+    image: postImage,
+    title: 'Гірське озеро',
+    comments: 1,
+    location: 'Chernivtsi Region, Ukraine',
+  },
+  {
+    id: 5,
+    image: postImage,
+    title: 'Карпатський пейзаж',
+    comments: 3,
+    location: 'Ternopil Region, Ukraine',
+  },
+];
