9b58c473bfc3f0dd84a375c5a57fff5c64dcde06
SalOne22 <111443297+SalOne22@users.noreply.github.com>
2023-08-15 11:23:07 +0200

    Added post list to posts screen

--- src/screens/PostsScreen.jsx
+++ src/screens/PostsScreen.jsx
@@ -1,15 +1,26 @@
+import { StyleSheet } from 'react-native';
 import { UserProfileInfo } from '../components/UserProfileInfo';
 import { ScrollContainer } from '../components/ScrollContainer';
+import { PostList } from '../components/PostList';
+import { posts } from '../mock/posts';
 import userProfileImage from '../../assets/images/userProfile.jpg';
 
 export default function PostsScreen() {
   return (
     <ScrollContainer>
       <UserProfileInfo
+        style={styles.userProfile}
         profileImage={userProfileImage}
         username="Natali Romanova"
         email="email@example.com"
       />
+      <PostList posts={posts} />
     </ScrollContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  userProfile: {
+    marginBottom: 32,
+  },
+});
