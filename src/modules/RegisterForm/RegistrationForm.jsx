97faafe07c2ec9d6389de7fe8a3f4d172dc78838
SalOne22 <111443297+SalOne22@users.noreply.github.com>
2023-08-17 16:05:04 +0200

    Added Registration form

---
+++ src/modules/RegisterForm/RegistrationForm.jsx
@@ -0,0 +1,124 @@
+import PropTypes from 'prop-types';
+import { useNavigation } from '@react-navigation/native';
+import { Controller, useForm } from 'react-hook-form';
+import { StyleSheet, View } from 'react-native';
+
+import userProfileImage from '@/assets/images/userProfile.jpg';
+
+import { ChangeableProfileImage } from '~/components/ChangeableProfileImage';
+import { Input } from '~/components/Input';
+import { PasswordInput } from '~/components/PasswordInput';
+
+import { Button } from '~/ui/Button';
+import { Header } from '~/ui/Header';
+import { ErrorText } from '~/ui/ErrorText';
+
+export const RegistrationForm = ({ style }) => {
+  const navigation = useNavigation();
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors, isValid },
+    reset,
+  } = useForm({
+    defaultValues: {
+      username: '',
+      email: '',
+      password: '',
+    },
+  });
+
+  const onSubmit = (data) => {
+    console.log(data);
+    reset();
+    navigation.navigate('Home');
+  };
+
+  return (
+    <View style={style}>
+      {/* Убрать source - появиться пустая картинка */}
+      <ChangeableProfileImage style={styles.userProfileImage} source={userProfileImage} />
+      <Header style={styles.headerText} title="Реєстрація" />
+
+      <View style={styles.input}>
+        <Controller
+          control={control}
+          rules={{
+            required: "Це поле - обов'язкове",
+            minLength: { value: 3, message: 'Мінімальна довжина - 3 символу' },
+          }}
+          render={({ field: { onChange, value } }) => (
+            <Input
+              autoComplete="username"
+              placeholder="Логін"
+              onChangeText={onChange}
+              value={value}
+            />
+          )}
+          name="username"
+        />
+        {errors.username && <ErrorText text={errors.username.message} />}
+      </View>
+
+      <View style={styles.input}>
+        <Controller
+          control={control}
+          rules={{
+            required: "Це поле - обов'язкове",
+            pattern: {
+              value: /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}\s*$/i,
+              message: 'Введіть валідний адрес пошти',
+            },
+          }}
+          render={({ field: { onChange, value } }) => (
+            <Input
+              autoComplete="email"
+              placeholder="Адреса електронної пошти"
+              onChangeText={onChange}
+              value={value}
+            />
+          )}
+          name="email"
+        />
+        {errors.email && <ErrorText text={errors.email.message} />}
+      </View>
+
+      <View style={styles.passwordInput}>
+        <Controller
+          control={control}
+          rules={{
+            required: "Це поле - обов'язкове",
+            minLength: { value: 6, message: 'Мінімальна довжина - 6 символів' },
+          }}
+          render={({ field: { onChange, value } }) => (
+            <PasswordInput onChangeText={onChange} value={value} />
+          )}
+          name="password"
+        />
+        {errors.password && <ErrorText text={errors.password.message} />}
+      </View>
+
+      <Button title="Зареєструватися" onPress={handleSubmit(onSubmit)} disabled={!isValid} />
+    </View>
+  );
+};
+
+RegistrationForm.propTypes = {
+  style: PropTypes.object,
+};
+
+const styles = StyleSheet.create({
+  userProfileImage: {
+    marginBottom: 32,
+  },
+  headerText: {
+    marginBottom: 33,
+  },
+  input: {
+    marginBottom: 16,
+  },
+  passwordInput: {
+    marginBottom: 43,
+  },
+});
--- src/screens/auth/RegistrationScreen.jsx
+++ src/screens/auth/RegistrationScreen.jsx
@@ -1,87 +1,23 @@
 import { useNavigation } from '@react-navigation/native';
-import { Controller, useForm } from 'react-hook-form';
 import { StyleSheet } from 'react-native';
 
 import bgImage from '@/assets/images/PhotoBG.jpg';
-import userProfileImage from '@/assets/images/userProfile.jpg';
-import { ChangeableProfileImage } from '~/components/ChangeableProfileImage';
-import { Input } from '~/components/Input';
-import { PasswordInput } from '~/components/PasswordInput';
+
+import { RegistrationForm } from '~/modules/RegisterForm';
+
 import { Background } from '~/ui/Background';
-import { Button } from '~/ui/Button';
-import { Header } from '~/ui/Header';
 import { LinkButton } from '~/ui/LinkButton';
 import { StaticModal } from '~/ui/StaticModal';
 
 export default function RegistrationScreen() {
   const navigation = useNavigation();
 
-  const { control, handleSubmit, reset } = useForm({
-    defaultValues: {
-      username: '',
-      email: '',
-      password: '',
-    },
-  });
-
-  const onSubmit = (data) => {
-    console.log(data);
-    reset();
-    navigation.navigate('Home');
-  };
-
   return (
     // Магическое число ✨
     // TODO: Надо заменить это на высоту от последнего инпута до конца контейнера
     <Background image={bgImage} verticalOffset={-160}>
       <StaticModal>
-        {/* Убрать source - появиться пустая картинка */}
-        <ChangeableProfileImage style={styles.userProfileImage} source={userProfileImage} />
-        <Header style={styles.headerText} title="Реєстрація" />
-        <Controller
-          control={control}
-          rules={{ required: true }}
-          render={({ field: { onChange, value } }) => (
-            <Input
-              style={styles.input}
-              autoComplete="username"
-              placeholder="Логін"
-              onChangeText={onChange}
-              value={value}
-            />
-          )}
-          name="username"
-        />
-
-        <Controller
-          control={control}
-          rules={{ required: true }}
-          render={({ field: { onChange, value } }) => (
-            <Input
-              style={styles.input}
-              autoComplete="email"
-              placeholder="Адреса електронної пошти"
-              onChangeText={onChange}
-              value={value}
-            />
-          )}
-          name="email"
-        />
-
-        <Controller
-          control={control}
-          rules={{ required: true }}
-          render={({ field: { onChange, value } }) => (
-            <PasswordInput style={styles.passwordInput} onChangeText={onChange} value={value} />
-          )}
-          name="password"
-        />
-
-        <Button
-          style={styles.registerButton}
-          title="Зареєструватися"
-          onPress={handleSubmit(onSubmit)}
-        />
+        <RegistrationForm style={styles.registrationForm} />
         <LinkButton onPress={() => navigation.navigate('Login')} textStyle={styles.loginButton}>
           Вже є аккаунт? Увійти
         </LinkButton>
@@ -91,19 +27,7 @@ export default function RegistrationScreen() {
 }
 
 const styles = StyleSheet.create({
-  userProfileImage: {
-    marginBottom: 32,
-  },
-  headerText: {
-    marginBottom: 33,
-  },
-  input: {
-    marginBottom: 16,
-  },
-  passwordInput: {
-    marginBottom: 43,
-  },
-  registerButton: {
+  registrationForm: {
     marginBottom: 16,
   },
   loginButton: {
