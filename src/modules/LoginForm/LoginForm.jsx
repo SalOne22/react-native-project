6877b5a8f1fb0509f1690ca2e2a771b29fa1d4c1
SalOne22 <111443297+SalOne22@users.noreply.github.com>
2023-08-17 15:38:38 +0200

    Added Login form

---
+++ src/modules/LoginForm/LoginForm.jsx
@@ -0,0 +1,86 @@
+import PropTypes from 'prop-types';
+import { useNavigation } from '@react-navigation/native';
+import { Controller, useForm } from 'react-hook-form';
+import { StyleSheet, View } from 'react-native';
+
+import { Input } from '~/components/Input';
+import { PasswordInput } from '~/components/PasswordInput';
+
+import { Header } from '~/ui/Header';
+import { Button } from '~/ui/Button';
+import { ErrorText } from '~/ui/ErrorText';
+
+export const LoginForm = ({ style }) => {
+  const navigation = useNavigation();
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  const onSubmit = (data) => {
+    console.log(data);
+    reset();
+    navigation.navigate('Home');
+  };
+
+  return (
+    <View style={style}>
+      <Header style={styles.header} title="Увійти" />
+
+      <View style={styles.input}>
+        <Controller
+          control={control}
+          rules={{ required: "Це поле - обов'язкове" }}
+          render={({ field: { onChange, value } }) => (
+            <Input
+              autoComplete="email"
+              placeholder="Адреса електронної пошти"
+              onChangeText={onChange}
+              value={value}
+            />
+          )}
+          name="email"
+        />
+        {errors.email && <ErrorText text={errors.email.message} />}
+      </View>
+
+      <View style={styles.password}>
+        <Controller
+          control={control}
+          rules={{ required: "Це поле - обов'язкове" }}
+          render={({ field: { onChange, value } }) => (
+            <PasswordInput onChangeText={onChange} value={value} />
+          )}
+          name="password"
+        />
+        {errors.password && <ErrorText text={errors.password.message} />}
+      </View>
+
+      <Button style={styles.loginButton} title="Увійти" onPress={handleSubmit(onSubmit)} />
+    </View>
+  );
+};
+
+LoginForm.propTypes = {
+  style: PropTypes.object,
+};
+
+const styles = StyleSheet.create({
+  header: {
+    marginBottom: 33,
+  },
+  input: {
+    marginBottom: 16,
+  },
+  password: {
+    marginBottom: 43,
+  },
+});
--- src/screens/auth/LoginScreen.jsx
+++ src/screens/auth/LoginScreen.jsx
@@ -1,64 +1,21 @@
 import { useNavigation } from '@react-navigation/native';
-import { Controller, useForm } from 'react-hook-form';
 import { StyleSheet, Text } from 'react-native';
 
 import bgImage from '@/assets/images/PhotoBG.jpg';
-import { Input } from '~/components/Input';
-import { PasswordInput } from '~/components/PasswordInput';
 import { Background } from '~/ui/Background';
-import { Button } from '~/ui/Button';
-import { Header } from '~/ui/Header';
 import { LinkButton } from '~/ui/LinkButton';
 import { StaticModal } from '~/ui/StaticModal';
+import { LoginForm } from '~/modules/LoginForm';
 
 export default function LoginScreen() {
   const navigation = useNavigation();
-
-  const { control, handleSubmit, reset } = useForm({
-    defaultValues: {
-      email: '',
-      password: '',
-    },
-  });
-
-  const onSubmit = (data) => {
-    console.log(data);
-    reset();
-    navigation.navigate('Home');
-  };
 
   return (
     // Магическое число ✨
     // TODO: Надо заменить это на высоту от последнего инпута до конца контейнера
     <Background image={bgImage} verticalOffset={-226}>
       <StaticModal style={styles.modal}>
-        <Header style={styles.header} title="Увійти" />
-
-        <Controller
-          control={control}
-          rules={{ required: true }}
-          render={({ field: { onChange, value } }) => (
-            <Input
-              style={styles.input}
-              autoComplete="email"
-              placeholder="Адреса електронної пошти"
-              onChangeText={onChange}
-              value={value}
-            />
-          )}
-          name="email"
-        />
-
-        <Controller
-          control={control}
-          rules={{ required: true }}
-          render={({ field: { onChange, value } }) => (
-            <PasswordInput style={styles.password} onChangeText={onChange} value={value} />
-          )}
-          name="password"
-        />
-
-        <Button style={styles.loginButton} title="Увійти" onPress={handleSubmit(onSubmit)} />
+        <LoginForm style={styles.loginForm} />
         <LinkButton
           onPress={() => navigation.navigate('Registration')}
           textStyle={styles.registrationButton}
@@ -75,16 +32,7 @@ const styles = StyleSheet.create({
     paddingTop: 32,
     paddingBottom: 111,
   },
-  header: {
-    marginBottom: 33,
-  },
-  input: {
-    marginBottom: 16,
-  },
-  password: {
-    marginBottom: 43,
-  },
-  loginButton: {
+  loginForm: {
     marginBottom: 16,
   },
   registrationButton: {
