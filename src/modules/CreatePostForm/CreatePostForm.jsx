190b07d8bd8c6174337a368556a3525fad833b1e
SalOne22 <111443297+SalOne22@users.noreply.github.com>
2023-08-17 16:23:28 +0200

    Added Create post form

---
+++ src/modules/CreatePostForm/CreatePostForm.jsx
@@ -0,0 +1,103 @@
+import PropTypes from 'prop-types';
+import { StyleSheet, View } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import { Controller, useForm } from 'react-hook-form';
+
+import postImage from '@/assets/images/postImage.jpg';
+
+import { ChangeablePostImage } from '~/components/ChangeablePostImage';
+import { GhostInput } from '~/components/GhostInput';
+import { DeleteButton } from '~/components/DeleteButton';
+
+import { Button } from '~/ui/Button';
+import { ErrorText } from '~/ui/ErrorText';
+
+export const CreatePostForm = ({ style }) => {
+  const navigation = useNavigation();
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors, isValid },
+    reset,
+  } = useForm({
+    defaultValues: {
+      title: '',
+      location: '',
+    },
+  });
+
+  const onSubmit = (data) => {
+    console.log(data);
+    reset();
+    navigation.navigate('Home');
+  };
+
+  return (
+    <View style={style}>
+      {/* Убрать source - появиться пустая картинка */}
+      <ChangeablePostImage style={styles.changeableImage} source={postImage} />
+
+      <View style={styles.topInput}>
+        <Controller
+          control={control}
+          rules={{ required: "Це поле - обов'язкове" }}
+          render={({ field: { onChange, value } }) => (
+            <GhostInput accent placeholder="Назва..." onChangeText={onChange} value={value} />
+          )}
+          name="title"
+        />
+        {errors.title && <ErrorText text={errors.title.message} />}
+      </View>
+
+      <View style={styles.bottomInput}>
+        <Controller
+          control={control}
+          rules={{ required: "Це поле - обов'язкове" }}
+          render={({ field: { onChange, value } }) => (
+            <GhostInput
+              placeholder="Місцевість..."
+              icon={<Feather name="map-pin" size={24} color="#dbdbdb" />}
+              onChangeText={onChange}
+              value={value}
+            />
+          )}
+          name="location"
+        />
+        {errors.location && <ErrorText text={errors.location.message} />}
+      </View>
+
+      <Button
+        style={styles.submitButton}
+        title="Опублікувати"
+        disabled={!isValid}
+        onPress={handleSubmit(onSubmit)}
+      />
+      <DeleteButton style={styles.deleteButton} onPress={() => reset()} />
+    </View>
+  );
+};
+
+CreatePostForm.propTypes = {
+  style: PropTypes.object,
+};
+
+const styles = StyleSheet.create({
+  changeableImage: {
+    marginBottom: 32,
+  },
+  topInput: {
+    marginBottom: 16,
+  },
+  bottomInput: {
+    marginBottom: 32,
+  },
+  submitButton: {
+    marginBottom: 32,
+  },
+  deleteButton: {
+    marginTop: 'auto',
+    alignSelf: 'center',
+  },
+});
--- src/screens/CreatePostsScreen.jsx
+++ src/screens/CreatePostsScreen.jsx
@@ -1,61 +1,18 @@
-import { Feather } from '@expo/vector-icons';
-import { useForm } from 'react-hook-form';
 import { StyleSheet } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import { CreatePostForm } from '~/modules/CreatePostForm';
 
-import postImage from '@/assets/images/postImage.jpg';
-import { ChangeablePostImage } from '~/components/ChangeablePostImage';
-import { DeleteButton } from '~/components/DeleteButton';
-import { GhostInput } from '~/components/GhostInput';
-import { Button } from '~/ui/Button';
 import { Container } from '~/ui/Container';
 
 export default function CreatePostsScreen() {
-  const navigation = useNavigation();
-
-  const { handleSubmit, reset } = useForm({
-    defaultValues: {
-      name: '',
-      location: '',
-    },
-  });
-
-  const onSubmit = (data) => {
-    console.log(data);
-    reset();
-    navigation.navigate('Home');
-  };
-
   return (
     <Container>
-      {/* Убрать source - появиться пустая картинка */}
-      <ChangeablePostImage style={styles.changeableImage} source={postImage} />
-
-      <GhostInput accent style={styles.topInput} placeholder="Назва..." />
-      <GhostInput
-        style={styles.bottomInput}
-        placeholder="Місцевість..."
-        icon={<Feather name="map-pin" size={24} color="#dbdbdb" />}
-      />
-
-      <Button title="Опублікувати" disabled onPress={handleSubmit(onSubmit)} />
-      <DeleteButton style={styles.deleteButton} onPress={() => reset()} />
+      <CreatePostForm style={styles.createPostForm} />
     </Container>
   );
 }
 
 const styles = StyleSheet.create({
-  changeableImage: {
-    marginBottom: 32,
-  },
-  topInput: {
-    marginBottom: 16,
-  },
-  bottomInput: {
-    marginBottom: 32,
-  },
-  deleteButton: {
-    marginTop: 'auto',
-    alignSelf: 'center',
+  createPostForm: {
+    height: '100%',
   },
 });
