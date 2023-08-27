3ac8118ff069e22b00ab957bf85d280248e747b8
SalOne22 <111443297+SalOne22@users.noreply.github.com>
2023-08-27 16:05:20 +0200

    Added Comments screen

--- src/screens/CommentsScreen.jsx
+++ src/screens/CommentsScreen.jsx
@@ -1,5 +1,21 @@
+import { StyleSheet } from 'react-native';
+import { useRoute } from '@react-navigation/native';
+
+import { PostImage } from '~/ui/PostImage';
 import { Container } from '~/ui/wrappers/Container';
+import { CommentList } from '~/modules/CommentList';
 
 export default function CommentsScreen() {
-  return <Container></Container>;
+  const {
+    params: { image, comments },
+  } = useRoute();
+
+  return (
+    <Container>
+      <PostImage style={styles.image} image={image} />
+      <CommentList comments={comments} />
+    </Container>
+  );
 }
+
+const styles = StyleSheet.create({ image: { marginBottom: 32 } });
