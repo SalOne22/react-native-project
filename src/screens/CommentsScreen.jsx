c0f57916043ac77c493298e199315b7ba6a6721a
SalOne22 <111443297+SalOne22@users.noreply.github.com>
2023-08-27 16:41:00 +0200

    Added comment form

--- src/screens/CommentsScreen.jsx
+++ src/screens/CommentsScreen.jsx
@@ -1,9 +1,10 @@
-import { StyleSheet } from 'react-native';
+import { Keyboard, StyleSheet, TouchableWithoutFeedback } from 'react-native';
 import { useRoute } from '@react-navigation/native';
 
 import { PostImage } from '~/ui/PostImage';
 import { Container } from '~/ui/wrappers/Container';
 import { CommentList } from '~/modules/CommentList';
+import { PostCommentForm } from '~/modules/PostCommentForm';
 
 export default function CommentsScreen() {
   const {
@@ -11,11 +12,17 @@ export default function CommentsScreen() {
   } = useRoute();
 
   return (
-    <Container>
-      <PostImage style={styles.image} image={image} />
-      <CommentList comments={comments} />
-    </Container>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <Container style={styles.container}>
+        <PostImage style={styles.image} image={image} />
+        <CommentList comments={comments} />
+        <PostCommentForm />
+      </Container>
+    </TouchableWithoutFeedback>
   );
 }
 
-const styles = StyleSheet.create({ image: { marginBottom: 32 } });
+const styles = StyleSheet.create({
+  container: { paddingBottom: 16 },
+  image: { marginBottom: 32 },
+});
