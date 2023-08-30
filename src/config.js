2a34296512c3e801a6f18ce071f7871c84c7f075
SalOne22 <111443297+SalOne22@users.noreply.github.com>
2023-08-30 18:50:18 +0200

    Added firebase

---
+++ src/config.js
@@ -0,0 +1,20 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDTLJSis4_MbcvLy-8T7TUJBzIQT6hHp64',
+  authDomain: 'react-native-project-47ba4.firebaseapp.com',
+  databaseURL: 'https://react-native-project-47ba4-default-rtdb.europe-west1.firebasedatabase.app',
+  projectId: 'react-native-project-47ba4',
+  storageBucket: 'react-native-project-47ba4.appspot.com',
+  messagingSenderId: '257778727491',
+  appId: '1:257778727491:web:c09933b8a55d9b8a1432f6',
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
